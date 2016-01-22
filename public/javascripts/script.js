0d13101083635d7cc8fc6817cf20007ce792403e
Tom <t.m.lemanski@googlemail.com>
2016-01-22 14:28:05 +0000

    First push of all files

---
+++ public/javascripts/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $("ul#header_list_item li").hover(function() {
+        $(this).toggleClass("active-button");
+    });
+    $('.responsive').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        dots: true,
+        rows: 2,
+        responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+        }, {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+        }, {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+        }]
+    });
+});
